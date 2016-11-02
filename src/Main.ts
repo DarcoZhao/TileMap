97ed69f700ec782db51c35f834718c67e81be447
613S025\sse <562019412@qq.com>
2016-11-02 11:29:04 +0800

    寻路仍有bug，在完善

---
+++ src/Main.ts
@@ -0,0 +1,414 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////、
+
+
+var playerAni =[ 
+    {tag:0,name: "Idle1_png"},
+    {tag:0,name: "Idle2_png"},
+    {tag:0,name: "Idle3_png"},
+    {tag:0,name: "Idle4_png"},
+    {tag:1,name: "Walk1_png"},
+    {tag:1,name: "Walk2_png"},
+    {tag:1,name: "Walk3_png"},
+    {tag:1,name: "Walk4_png"},
+]
+
+
+
+class MyPlayer extends egret.DisplayObjectContainer {
+    
+    public nowState=0;
+    public MyPhoto:egret.Bitmap;
+    private MySta:StaMac=new StaMac;
+    public MoveSpeed:number=25;
+    public Modle:number=0;
+    public IdleAni:Array<egret.Texture>=new Array<egret.Texture>();
+    public MoveAni:Array<egret.Texture>=new Array<egret.Texture>();
+    public constructor(){
+       super();
+       this.MyPhoto=this.createBitmapByName("1_png");
+       this.addChild(this.MyPhoto);
+       this.LoadAni();
+       this.anchorOffsetX=this.MyPhoto.width/2;
+       this.anchorOffsetY=this.MyPhoto.height/2;
+   }
+
+    private LoadAni() {
+        var texture:egret.Texture ;
+        for(var i=0; i<playerAni.length; i++) {
+             texture= RES.getRes(playerAni[i].name);
+             if(playerAni[i].tag==0){this.IdleAni.push(texture); }
+             if(playerAni[i].tag==1){this.MoveAni.push(texture); }
+        }
+    }
+    
+//不太懂？？
+    public PlayAni(Ani:Array<egret.Texture>){
+
+        var count =0;
+        var Bit=this.MyPhoto;
+        var M=this.Modle;
+        //console.log("M:"+M);
+        var timer:egret.Timer=new egret.Timer(125, 0);
+        timer.addEventListener(egret.TimerEvent.TIMER,Play,this);
+        timer.start();
+
+        function Play(){      
+            Bit.texture=Ani[count];
+            if(count<Ani.length-1) {count++;}
+            else{count=0;}    
+            if(this.Modle!=M){
+                //console.log("tM:"+M+" nowM:"+this.Modle); 
+                timer.stop();
+            }    
+        }   
+  
+    }
+
+    public Move(Ps:Point[],xx:number,yy:number) {
+        var MS:MoveSta=new MoveSta(Ps,this);
+        this.MySta.Reload(MS);      
+    }
+    
+
+    public Idle() {
+       var IS:IdleSta=new IdleSta(this);
+       this.MySta.Reload(IS);
+   }
+
+/**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name:string):egret.Bitmap {
+        var result = new egret.Bitmap();
+        var texture:egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+}
+
+interface  Sta {
+    Load();
+    exit();
+     
+}
+
+class MoveSta implements Sta{
+    private Tx:number;
+    private Ty:number;
+    private Player:MyPlayer;
+    private timer: egret.Timer;
+    private LeastTime:number;
+    public Path:Point[];
+    public nowNode:number;
+
+    public ArriveListener:egret.Sprite=new egret.Sprite();  //Sprite是一个显示列表节点
+   
+
+    constructor(Ps:Point[],Player:MyPlayer){
+        this.Path=Ps;
+        this.Player=Player;
+    }
+
+
+     Move() {
+          
+        this.nowNode++;
+        if(this.nowNode<this.Path.length){
+            var M=this.Player.Modle;     
+            this.Tx=this.Path[this.nowNode].x;
+            this.Ty=this.Path[this.nowNode].y;
+            var xx=this.Tx- this.Player.x;
+            var yy=this.Ty- this.Player.y;
+            if(xx>0){this.Player.scaleX=-1;}else{this.Player.scaleX=1;}
+            var zz=Math.pow(xx*xx+yy*yy,0.5);
+            var time:number=zz/this.Player.MoveSpeed;
+            this.timer = new egret.Timer(50, time);
+            this.LeastTime=time;
+            this.timer.start();
+        
+            this.timer.addEventListener(egret.TimerEvent.TIMER,()=>{
+                this.Player.x+=xx/time;
+                this.Player.y+=yy/time;
+                this.LeastTime--;
+                if( this.LeastTime<1) {
+                    this.timer.stop();
+                    if(this.LeastTime>-10) { 
+                    this.Move();
+                    //  var IFW:FinishWalkEvent=new FinishWalkEvent(FinishWalkEvent.FW);
+                    //   this.ArriveListener.dispatchEvent(IFW);
+                    }//意味着是走停不是逼停
+                }
+            }, this);
+        }else {
+            this.Player.Idle();
+            return;
+        }
+    }
+
+    Load() {
+        if(this.Path.length>1)
+        this.nowNode=1;
+        else this.nowNode=0;
+        this.Player.Modle++;
+        this.Player.PlayAni(this.Player.MoveAni);
+        this.Move();
+    }
+
+    exit() {
+        this.LeastTime=-10;
+    }
+}
+
+class IdleSta implements Sta{
+      private Player:MyPlayer;
+      constructor(Player:MyPlayer){  
+        this.Player=Player;
+    }  
+    Load() {
+        this.Player.Modle++;
+        this.Player.nowState=0;
+        this.Player.PlayAni(this.Player.IdleAni);
+    }
+    exit() {
+        console.log("exitIdle");
+    }
+
+}
+ class StaMac {
+     private nowSta:Sta;
+     
+     public Reload(S:Sta):void{
+         if(this.nowSta){this.nowSta.exit();}  
+        this.nowSta=S;
+        this.nowSta.Load();
+     }
+}
+
+
+
+
+class Main extends egret.DisplayObjectContainer {
+
+    /**
+     * 加载进度界面
+     * Process interface loading
+     */
+    private loadingView:LoadingUI;
+    private Player:MyPlayer;
+    private BgMap:Grid=new Grid();
+
+    public constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    private onAddToStage(event:egret.Event) {
+        //设置加载进度界面
+        //Config to load process interface
+        this.loadingView = new LoadingUI();
+        this.stage.addChild(this.loadingView);
+
+        //初始化Resource资源加载库
+        //initiate Resource loading library
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/default.res.json", "resource/");
+    }
+
+    /**
+     * 配置文件加载完成,开始预加载preload资源组。
+     * configuration file loading is completed, start to pre-load the preload resource group
+     */
+    private onConfigComplete(event:RES.ResourceEvent):void {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+        RES.loadGroup("preload");
+    }
+
+    /**
+     * preload资源组加载完成
+     * Preload resource group is loaded
+     */
+    private onResourceLoadComplete(event:RES.ResourceEvent):void {
+        if (event.groupName == "preload") {
+            this.stage.removeChild(this.loadingView);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+            this.createGameScene();
+        }
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onItemLoadError(event:RES.ResourceEvent):void {
+        console.warn("Url:" + event.resItem.url + " has failed to load");
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onResourceLoadError(event:RES.ResourceEvent):void {
+        //TODO
+        console.warn("Group:" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        //Ignore the loading failed projects
+        this.onResourceLoadComplete(event);
+    }
+
+    /**
+     * preload资源组加载进度
+     * Loading process of preload resource group
+     */
+    private onResourceProgress(event:RES.ResourceEvent):void {
+        if (event.groupName == "preload") {
+            this.loadingView.setProgress(event.itemsLoaded, event.itemsTotal);
+        }
+    }
+
+    private textfield:egret.TextField;
+
+    /**
+     * 创建游戏场景
+     * Create a game scene
+     */
+    private createGameScene():void {
+        var bg:egret.Bitmap = this.createBitmapByName("Sky_jpeg");
+        this.addChild(bg);
+        var stageW:number = this.stage.stageWidth;
+        var stageH:number = this.stage.stageHeight;
+        bg.width = stageW;
+        bg.height = stageH;
+
+        this.BgMap = new Grid();
+        this.Player = new MyPlayer();
+        printMap(this.BgMap);
+        this.addChild(this.BgMap);
+
+        this.addChild(this.Player);
+        this.Player.x= this.Player.y=20;
+        this.Player.Idle();
+        this.touchEnabled=true;
+        this.addEventListener(egret.TouchEvent.TOUCH_TAP,this.Click,this);
+    }
+
+    private Click(evt:egret.TouchEvent):void {   
+        var sp:Point=new Point();
+        var ep:Point=new Point();
+        sp.x=this.Player.x;
+        sp.y=this.Player.y;
+        ep.x=Math.floor(evt.stageX/64);
+        ep.y=Math.floor(evt.stageY/64);
+        var As=new Astar(sp,ep);
+        this.Player.Move(As.Ps,10,10);  //?????
+        //this.Player.Move(As.Ps);  
+    }
+
+
+
+
+
+
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name:string):egret.Bitmap {
+        var result = new egret.Bitmap();
+        var texture:egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    //////////////////////////////////////////////////////////////////////////
+    /*
+    private startAnimation(result:Array<any>):void {
+        var self:any = this;
+
+        var parser = new egret.HtmlTextParser();
+        var textflowArr:Array<Array<egret.ITextElement>> = [];
+        for (var i:number = 0; i < result.length; i++) {
+            textflowArr.push(parser.parser(result[i]));
+        }
+
+        var textfield = self.textfield;
+        var count = -1;
+        var change:Function = function () {
+            count++;
+            if (count >= textflowArr.length) {
+                count = 0;
+            }
+            var lineArr = textflowArr[count];
+
+            self.changeDescription(textfield, lineArr);
+
+            var tw = egret.Tween.get(textfield);
+            tw.to({"alpha": 1}, 200);
+            tw.wait(2000);
+            tw.to({"alpha": 0}, 200);
+            tw.call(change, self);
+        };
+
+        change();
+    }
+
+    */
+    //////////////////////////////////////////////////////////////////////
+
+
+    /**
+     * 切换描述内容
+     * Switch to described content
+     */
+    private changeDescription(textfield:egret.TextField, textFlow:Array<egret.ITextElement>):void {
+        textfield.textFlow = textFlow;
+    }
+}
+
+
+class FinishWalkEvent extends egret.Event
+{
+    public static FW:string = "Finished";
+    public constructor(type:string, bubbles:boolean=false, cancelable:boolean=false)
+    {
+        super(type,bubbles,cancelable);
+    }
+}
---
+++ src/Map.ts
@@ -0,0 +1,421 @@
+// TypeScript file
+
+var grid = [
+    {x:0,y:0,walkable:true,img:"Bg_01_png"},
+    {x:1,y:0,walkable:true,img:"Bg_02_png"},
+    {x:2,y:0,walkable:true,img:"Bg_03_png"},
+    {x:3,y:0,walkable:true,img:"Bg_04_png"},
+    {x:4,y:0,walkable:true,img:"Bg_05_png"},
+    {x:5,y:0,walkable:true,img:"Bg_06_png"},
+    {x:6,y:0,walkable:true,img:"Bg_07_png"},
+    {x:7,y:0,walkable:true,img:"Bg_08_png"},
+    {x:8,y:0,walkable:true,img:"Bg_09_png"},
+    {x:9,y:0,walkable:true,img:"Bg_10_png"},
+
+
+    {x:0,y:1,walkable:true,img:"Bg_11_png"},
+    {x:1,y:1,walkable:true,img:"Bg_12_png"},
+    {x:2,y:1,walkable:true,img:"Bg_13_png"},
+    {x:3,y:1,walkable:true,img:"Bg_14_png"},
+    {x:4,y:1,walkable:true,img:"Bg_15_png"},
+    {x:5,y:1,walkable:true,img:"Bg_16_png"},
+    {x:6,y:1,walkable:true,img:"Bg_17_png"},
+    {x:7,y:1,walkable:true,img:"Bg_18_png"},
+    {x:8,y:1,walkable:true,img:"Bg_19_png"},
+    {x:9,y:1,walkable:true,img:"Bg_20_png"},
+
+    {x:0,y:2,walkable:true,img:"Bg_21_png"},
+    {x:1,y:2,walkable:true,img:"Bg_22_png"},
+    {x:2,y:2,walkable:true,img:"Bg_23_png"},
+    {x:3,y:2,walkable:true,img:"Bg_24_png"},
+    {x:4,y:2,walkable:true,img:"Bg_25_png"},
+    {x:5,y:2,walkable:true,img:"Bg_26_png"},
+    {x:6,y:2,walkable:true,img:"Bg_27_png"},
+    {x:7,y:2,walkable:true,img:"Bg_28_png"},
+    {x:8,y:2,walkable:true,img:"Bg_29_png"},
+    {x:9,y:2,walkable:true,img:"Bg_30_png"},
+
+    {x:0,y:3,walkable:true,img:"Bg_31_png"},
+    {x:1,y:3,walkable:true,img:"Bg_32_png"},
+    {x:2,y:3,walkable:true,img:"Bg_33_png"},
+    {x:3,y:3,walkable:true,img:"Bg_34_png"},
+    {x:4,y:3,walkable:true,img:"Bg_35_png"},
+    {x:5,y:3,walkable:true,img:"Bg_36_png"},
+    {x:6,y:3,walkable:true,img:"Bg_37_png"},
+    {x:7,y:3,walkable:true,img:"Bg_38_png"},
+    {x:8,y:3,walkable:true,img:"Bg_39_png"},
+    {x:9,y:3,walkable:true,img:"Bg_40_png"},
+
+    {x:0,y:4,walkable:true,img:"Bg_41_png"},
+    {x:1,y:4,walkable:true,img:"Bg_42_png"},
+    {x:2,y:4,walkable:true,img:"Bg_43_png"},
+    {x:3,y:4,walkable:true,img:"Bg_44_png"},
+    {x:4,y:4,walkable:true,img:"Bg_45_png"},
+    {x:5,y:4,walkable:true,img:"Bg_46_png"},
+    {x:6,y:4,walkable:true,img:"Bg_47_png"},
+    {x:7,y:4,walkable:true,img:"Bg_48_png"},
+    {x:8,y:4,walkable:true,img:"Bg_49_png"},
+    {x:9,y:4,walkable:true,img:"Bg_50_png"},
+
+    {x:0,y:5,walkable:true,img:"Bg_51_png"},
+    {x:1,y:5,walkable:true,img:"Bg_52_png"},
+    {x:2,y:5,walkable:true,img:"Bg_53_png"},
+    {x:3,y:5,walkable:true,img:"Bg_54_png"},
+    {x:4,y:5,walkable:true,img:"Bg_55_png"},
+    {x:5,y:5,walkable:true,img:"Bg_56_png"},
+    {x:6,y:5,walkable:true,img:"Bg_57_png"},
+    {x:7,y:5,walkable:true,img:"Bg_58_png"},
+    {x:8,y:5,walkable:true,img:"Bg_59_png"},
+    {x:9,y:5,walkable:true,img:"Bg_60_png"},
+
+    {x:0,y:6,walkable:true,img:"Bg_61_png"},
+    {x:1,y:6,walkable:true,img:"Bg_62_png"},
+    {x:2,y:6,walkable:true,img:"Bg_63_png"},
+    {x:3,y:6,walkable:true,img:"Bg_64_png"},
+    {x:4,y:6,walkable:true,img:"Bg_65_png"},
+    {x:5,y:6,walkable:true,img:"Bg_66_png"},
+    {x:6,y:6,walkable:true,img:"Bg_67_png"},
+    {x:7,y:6,walkable:true,img:"Bg_68_png"},
+    {x:8,y:6,walkable:true,img:"Bg_69_png"},
+    {x:9,y:6,walkable:true,img:"Bg_70_png"},
+
+    {x:0,y:7,walkable:true,img:"Bg_71_png"},
+    {x:1,y:7,walkable:true,img:"Bg_72_png"},
+    {x:2,y:7,walkable:true,img:"Bg_73_png"},
+    {x:3,y:7,walkable:true,img:"Bg_74_png"},
+    {x:4,y:7,walkable:true,img:"Bg_75_png"},
+    {x:5,y:7,walkable:true,img:"Bg_76_png"},
+    {x:6,y:7,walkable:true,img:"Bg_77_png"},
+    {x:7,y:7,walkable:true,img:"Bg_78_png"},
+    {x:8,y:7,walkable:true,img:"Bg_79_png"},
+    {x:9,y:7,walkable:true,img:"Bg_80_png"},
+
+    {x:0,y:8,walkable:true,img:"Bg_81_png"},
+    {x:1,y:8,walkable:true,img:"Bg_82_png"},
+    {x:2,y:8,walkable:true,img:"Bg_83_png"},
+    {x:3,y:8,walkable:true,img:"Bg_84_png"},
+    {x:4,y:8,walkable:true,img:"Bg_85_png"},
+    {x:5,y:8,walkable:true,img:"Bg_86_png"},
+    {x:6,y:8,walkable:true,img:"Bg_87_png"},
+    {x:7,y:8,walkable:true,img:"Bg_88_png"},
+    {x:8,y:8,walkable:true,img:"Bg_89_png"},
+    {x:9,y:8,walkable:true,img:"Bg_90_png"},
+
+    {x:0,y:9,walkable:true,img:"Bg_91_png"},
+    {x:1,y:9,walkable:true,img:"Bg_92_png"},
+    {x:2,y:9,walkable:true,img:"Bg_93_png"},
+    {x:3,y:9,walkable:true,img:"Bg_94_png"},
+    {x:4,y:9,walkable:true,img:"Bg_95_png"},
+    {x:5,y:9,walkable:true,img:"Bg_96_png"},
+    {x:6,y:9,walkable:true,img:"Bg_97_png"},
+    {x:7,y:9,walkable:true,img:"Bg_98_png"},
+    {x:8,y:9,walkable:true,img:"Bg_99_png"},
+    {x:9,y:9,walkable:true,img:"Bg_100_png"},
+
+    {x:0,y:10,walkable:true,img:"Bg_101_png"},
+    {x:1,y:10,walkable:true,img:"Bg_102_png"},
+    {x:2,y:10,walkable:true,img:"Bg_103_png"},
+    {x:3,y:10,walkable:true,img:"Bg_104_png"},
+    {x:4,y:10,walkable:true,img:"Bg_105_png"},
+    {x:5,y:10,walkable:true,img:"Bg_106_png"},
+    {x:6,y:10,walkable:true,img:"Bg_107_png"},
+    {x:7,y:10,walkable:true,img:"Bg_108_png"},
+    {x:8,y:10,walkable:true,img:"Bg_109_png"},
+    {x:9,y:10,walkable:true,img:"Bg_110_png"},
+
+    {x:0,y:11,walkable:true,img:"Bg_111_png"},
+    {x:1,y:11,walkable:true,img:"Bg_112_png"},
+    {x:2,y:11,walkable:true,img:"Bg_113_png"},
+    {x:3,y:11,walkable:true,img:"Bg_114_png"},
+    {x:4,y:11,walkable:true,img:"Bg_115_png"},
+    {x:5,y:11,walkable:true,img:"Bg_116_png"},
+    {x:6,y:11,walkable:true,img:"Bg_117_png"},
+    {x:7,y:11,walkable:true,img:"Bg_118_png"},
+    {x:8,y:11,walkable:true,img:"Bg_119_png"},
+    {x:9,y:11,walkable:true,img:"Bg_120_png"},
+
+    {x:0,y:12,walkable:true,img:"Bg_121_png"},
+    {x:1,y:12,walkable:true,img:"Bg_122_png"},
+    {x:2,y:12,walkable:true,img:"Bg_123_png"},
+    {x:3,y:12,walkable:true,img:"Bg_124_png"},
+    {x:4,y:12,walkable:true,img:"Bg_125_png"},
+    {x:5,y:12,walkable:true,img:"Bg_126_png"},
+    {x:6,y:12,walkable:true,img:"Bg_127_png"},
+    {x:7,y:12,walkable:true,img:"Bg_128_png"},
+    {x:8,y:12,walkable:true,img:"Bg_129_png"},
+    {x:9,y:12,walkable:true,img:"Bg_130_png"},
+
+    {x:0,y:13,walkable:true,img:"Bg_131_png"},
+    {x:1,y:13,walkable:true,img:"Bg_132_png"},
+    {x:2,y:13,walkable:true,img:"Bg_133_png"},
+    {x:3,y:13,walkable:true,img:"Bg_134_png"},
+    {x:4,y:13,walkable:true,img:"Bg_135_png"},
+    {x:5,y:13,walkable:true,img:"Bg_136_png"},
+    {x:6,y:13,walkable:true,img:"Bg_137_png"},
+    {x:7,y:13,walkable:true,img:"Bg_138_png"},
+    {x:8,y:13,walkable:true,img:"Bg_139_png"},
+    {x:9,y:13,walkable:true,img:"Bg_140_png"},
+
+    {x:0,y:14,walkable:true,img:"Bg_141_png"},
+    {x:1,y:14,walkable:true,img:"Bg_142_png"},
+    {x:2,y:14,walkable:true,img:"Bg_143_png"},
+    {x:3,y:14,walkable:true,img:"Bg_144_png"},
+    {x:4,y:14,walkable:true,img:"Bg_145_png"},
+    {x:5,y:14,walkable:true,img:"Bg_146_png"},
+    {x:6,y:14,walkable:true,img:"Bg_147_png"},
+    {x:7,y:14,walkable:true,img:"Bg_148_png"},
+    {x:8,y:14,walkable:true,img:"Bg_149_png"},
+    {x:9,y:14,walkable:true,img:"Bg_150_png"},
+
+    {x:0,y:15,walkable:true,img:"Bg_151_png"},
+    {x:1,y:15,walkable:true,img:"Bg_152_png"},
+    {x:2,y:15,walkable:true,img:"Bg_153_png"},
+    {x:3,y:15,walkable:true,img:"Bg_154_png"},
+    {x:4,y:15,walkable:true,img:"Bg_155_png"},
+    {x:5,y:15,walkable:true,img:"Bg_156_png"},
+    {x:6,y:15,walkable:true,img:"Bg_157_png"},
+    {x:7,y:15,walkable:true,img:"Bg_158_png"},
+    {x:8,y:15,walkable:true,img:"Bg_159_png"},
+    {x:9,y:15,walkable:true,img:"Bg_160_png"},
+
+    {x:0,y:16,walkable:true,img:"Bg_161_png"},
+    {x:1,y:16,walkable:true,img:"Bg_162_png"},
+    {x:2,y:16,walkable:true,img:"Bg_163_png"},
+    {x:3,y:16,walkable:true,img:"Bg_164_png"},
+    {x:4,y:16,walkable:true,img:"Bg_165_png"},
+    {x:5,y:16,walkable:true,img:"Bg_166_png"},
+    {x:6,y:16,walkable:true,img:"Bg_167_png"},
+    {x:7,y:16,walkable:true,img:"Bg_168_png"},
+    {x:8,y:16,walkable:true,img:"Bg_169_png"},
+    {x:9,y:16,walkable:true,img:"Bg_170_png"},    
+]
+
+class Grid extends egret.DisplayObjectContainer{
+    public onePSize=64;
+    public ArrayGrid:One[]=[];
+    public W=10;
+    public H=17;
+   
+    constructor(){
+        super();
+        this.Load();     
+    }
+     private createBitmapByName(name:string):egret.Bitmap {
+        var result = new egret.Bitmap();
+        var texture:egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+    private Load() {
+
+        for(var i=0;i<grid.length;i++) {
+            var OneP:One=new One();
+            OneP.x=grid[i].x;   
+            OneP.y=grid[i].y;  
+            OneP.x=grid[i].x*64;   
+            OneP.y=grid[i].y*64; 
+            this.ArrayGrid.push(OneP);
+            //this.addChild(OneP);
+        }
+    }  
+}
+
+
+class Point {
+    public x:number;
+    public y:number;
+}
+
+ function printMap(gg:Grid):void {   
+    var x="";
+    var i=0,j=0,k=0;
+    for(;j<gg.H;j++){
+        x="";
+        for(i=0;i<gg.W;i++) {
+            if(gg.ArrayGrid[k].walkable==true)
+                x=x+"口";
+                else { x=x+"国";}
+            k++;
+         }
+         console.log(x);
+    }  
+}
+
+
+class One extends egret.DisplayObjectContainer {
+    x:number;
+    y:number;
+    walkable:boolean;
+    img:string;
+}
+
+class AStarP {
+    public point:Point=new Point();
+    public f:number;
+    public g:number;
+    public h:number;
+    public frontG:AStarP;
+}
+
+class Astar {
+    public EndP:AStarP=new AStarP();
+    private nowP:AStarP=new AStarP();
+    public Ps:Point[]=[];
+    private O:AStarP[]=[];
+    private C:AStarP[]=[];
+    private EndNcanW=true;
+    private IsOk=false;
+    constructor(sp:Point,np:Point) {
+        this.Ps=[];
+        this.O=[];
+        this.C=[];
+        this.EndP.point.x=Math.floor(np.x/64);//取整
+        this.EndP.point.y=Math.floor(np.y/64);
+        this.nowP.point.x=Math.floor(sp.x/64);
+        this.nowP.point.y=Math.floor(sp.y/64);
+        this.nowP.g=0;
+        this.nowP.frontG=this.nowP;
+        this.sh(this.nowP);  //计算h和f
+        this.sf(this.nowP);
+        this.O.push(this.nowP);    //O表，开放列表
+         if(this.isF(this.EndP)) {   //这个点不能走
+             this.EndNcanW=false;
+         }
+
+         if(this.nowP.point.x==this.EndP.point.x&&this.nowP.point.y==this.EndP.point.y) {
+             this.IsOk=true;
+             this.EndP.frontG=this.nowP;
+         }
+         
+         
+         var t=0;
+         do{
+            this.nowP=this.getP();
+            this.searchSur(this.nowP);
+            this.C.push(this.nowP);    
+            t++;
+            if(this.O.length<=0 ||this.IsOk) {break;}
+        
+        }while(this.EndNcanW);
+
+        var reversePath:AStarP[]=[];
+        if(this.EndNcanW) {
+          reversePath.push(this.EndP);
+        }
+        do{
+            reversePath.push(this.nowP);
+         
+            //翻转完，break，跳出do的循环
+            if(this.nowP.frontG.point.x==this.nowP.point.x&&this.nowP.frontG.point.y==this.nowP.point.y){break;}
+            this.nowP=this.nowP.frontG; 
+        }while(this.EndNcanW);
+
+
+        var j=0;
+        for(var i=reversePath.length;i>0;i--) {
+            var op:Point=new Point();
+            op.x=reversePath[i-1].point.x;
+            op.y=reversePath[i-1].point.y;
+            this.Ps.push(op);
+   
+            this.Ps[j].x=this.Ps[j].x*64;
+            this.Ps[j].y=this.Ps[j].y*64;
+            j++;
+        }
+
+    }
+
+        
+    public searchSur(n:AStarP) {
+        this.AddNtoO(n,n.point.x-1,n.point.y-1);
+        this.AddNtoO(n,n.point.x,n.point.y-1);
+        this.AddNtoO(n,n.point.x+1,n.point.y-1);
+        this.AddNtoO(n,n.point.x-1,n.point.y);
+        this.AddNtoO(n,n.point.x+1,n.point.y);
+        this.AddNtoO(n,n.point.x-1,n.point.y+1);
+        this.AddNtoO(n,n.point.x,n.point.y+1);
+        this.AddNtoO(n,n.point.x+1,n.point.y+1);
+    }
+
+
+    public u=0;
+    public AddNtoO(fn:AStarP,x:number,y:number) {
+        if(x<0||y<0||x>=10||y>=18){return;}
+        var n:AStarP=new AStarP();
+        n.point.x=x;
+        n.point.y=y;
+
+        if(this.IsinC(n)||this.isF(n)||this.IsinO(n)) {
+            return ;
+        }else {
+            n.frontG=fn;
+            this.sg(n);this.sh(n);this.sf(n);
+            this.O.push(n);
+
+            this.u++;
+    
+            if(this.EndP.point.x==n.point.x&&this.EndP.point.y==n.point.y) {
+                this.IsOk=true;
+            }
+        }
+
+    }
+
+    
+    public IsinO(n:AStarP):boolean{
+        for(var i=0;i<this.O.length;i++) {
+            if(n.point.x==this.O[i].point.x&&n.point.y==this.O[i].point.y)
+                return true;
+        }
+        return false ;
+    }
+    public IsinC(n:AStarP):boolean{
+        for(var i=0;i<this.C.length;i++) {
+            if(n.point.x==this.C[i].point.x&&n.point.y==this.C[i].point.y)
+                return true;
+        }
+        return false ;
+        
+    }
+    public isF(n:AStarP):boolean{
+        for(var i=0;i<grid.length;i++) {
+            if(n.point.x==grid[i].x&&n.point.y==grid[i].y&&grid[i].walkable==false) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public sh(n:AStarP) {
+        var xx=Math.max(n.point.x,this.EndP.point.x)-Math.min(n.point.x,this.EndP.point.x);
+        var yy=Math.max(n.point.y,this.EndP.point.y)-Math.min(n.point.y,this.EndP.point.y);
+        n.h=xx+yy;
+    }
+    public sg(n:AStarP) {
+       if(n.point.x!=n.frontG.point.x&&n.point.y !=n.frontG.point.y) {
+           n.g=n.frontG.g+1.4;
+       }else {
+           n.g=n.frontG.g+1;
+       }
+       
+    }
+    public sf(n:AStarP) {
+        n.f=n.g+n.h;
+    }
+    public getP():AStarP{ 
+        var gn:AStarP;
+        var gf=999;
+        var k:number;
+        for(var i=0;i<this.O.length;i++) {
+            if(this.O[i].f<gf) {
+                gf=this.O[i].f;
+                k=i;
+            }
+        }
+        gn=this.O[k];
+        this.O[k]=this.O[0];
+        this.O[0]=gn;      
+        return this.O.shift();
+    }
+
+}
+
